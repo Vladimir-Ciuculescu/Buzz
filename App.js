8efac33475b76f5bfcf26e9162247a7273444e08
Riyokoo <vladimir.ciuculescu@titeica.com>
2021-10-14 13:08:36 +0300

    Added the navigation root and firebase app

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import firebase from "firebase";
+
+//screens
+import HomeScreen from "./screens/HomeScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDGSqKcZcb6dPTEYPvhbbBZhB9DBTZwe6Y",
+  authDomain: "licenta-ec832.firebaseapp.com",
+  projectId: "licenta-ec832",
+  storageBucket: "licenta-ec832.appspot.com",
+  messagingSenderId: "752828941053",
+  appId: "1:752828941053:web:615a29ebce3c967d40b494",
+  measurementId: "G-8DLE0JFN63",
+};
+
+//If no firebase app has been initialized when the component is rendered, initialize it
+// Else use the one initialized already
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+const AppStack = createStackNavigator({
+  Home: HomeScreen,
 });
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen,
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppStack,
+      Auth: AuthStack,
+    },
+    {
+      initialRouteName: "Loading",
+    }
+  )
+);
---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDGSqKcZcb6dPTEYPvhbbBZhB9DBTZwe6Y",
+  authDomain: "licenta-ec832.firebaseapp.com",
+  projectId: "licenta-ec832",
+  storageBucket: "licenta-ec832.appspot.com",
+  messagingSenderId: "752828941053",
+  appId: "1:752828941053:web:615a29ebce3c967d40b494",
+  measurementId: "G-8DLE0JFN63",
+};
+
+firebase.initializeApp(firebaseConfig);
