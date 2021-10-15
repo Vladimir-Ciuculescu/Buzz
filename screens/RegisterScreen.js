42bb60a3549cbe889e51ff7162873119fed4810d
Riyokoo <vladimir.ciuculescu@titeica.com>
2021-10-15 23:49:54 +0300

    Added logo and pictures on login and register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,7 @@
 import React from "react";
+import firebase from "firebase";
+import firetore from "firebase/firestore";
+
 import {
   View,
   Text,
@@ -6,18 +9,73 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
+  ActivityIndicator,
+  Alert,
+  Image,
 } from "react-native";
 
 export default class RegisterScreen extends React.Component {
   state = {
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
+    repeatPassword: "",
+    registerError: "",
+    existAccountLoading: false,
+    createAccountLoading: false,
+  };
+
+  register = async () => {
+    const { firstName, lastName, email, password, repeatPassword } = this.state;
+
+    var existentAccount = false;
+    this.setState({ existAccountLoading: true });
+    this.setState({ createAccountLoading: true });
+
+    const accounts = await firebase.firestore().collection("accounts").get();
+
+    this.setState({ existAccountLoading: false });
+
+    for (const doc of accounts.docs) {
+      if (email === doc.data().email && password === doc.data().password) {
+        existentAccount = true;
+      }
+    }
+
+    if (existentAccount === true) {
+      this.setState({ registerError: "This account already exists !" });
+    } else {
+      const fullName = lastName + " " + firstName;
+      firebase.firestore().collection("accounts").doc(fullName).set({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      });
+      this.setState({ createAccountLoading: false });
+      Alert.alert("Success", "Account succesfully created", [
+        {
+          text: "OK",
+          onPress: () => this.props.navigation.navigate("Login"),
+        },
+      ]);
+    }
   };
 
   render() {
     return (
       <View style={styles.container}>
         <ScrollView>
+          <Image
+            source={require("../assets/green_circle.png.png")}
+            style={styles.greenCircle}
+          ></Image>
+          <Image
+            source={require("../assets/Yellow_icon.svg.png")}
+            style={styles.yellowCircle}
+          ></Image>
+
           <Text style={styles.registerMessage}>Register account</Text>
 
           <View style={styles.form}>
@@ -26,8 +84,8 @@ export default class RegisterScreen extends React.Component {
               <TextInput
                 underlineColorAndroid="transparent"
                 style={styles.input}
-                onChangeText={(email) => this.setState({ email })}
-                value={this.state.email}
+                onChangeText={(firstName) => this.setState({ firstName })}
+                value={this.state.firstName}
               ></TextInput>
             </View>
 
@@ -36,8 +94,8 @@ export default class RegisterScreen extends React.Component {
               <TextInput
                 underlineColorAndroid="transparent"
                 style={styles.input}
-                onChangeText={(email) => this.setState({ email })}
-                value={this.state.email}
+                onChangeText={(lastName) => this.setState({ lastName })}
+                value={this.state.lastName}
               ></TextInput>
             </View>
 
@@ -46,11 +104,10 @@ export default class RegisterScreen extends React.Component {
               <TextInput
                 placeholder="Your email address should have @vspartners.us"
                 underlineColorAndroid="transparent"
-                secureTextEntry
                 autoCapitalize="none"
                 style={styles.input}
-                onChangeText={(password) => this.setState({ password })}
-                value={this.state.password}
+                onChangeText={(email) => this.setState({ email })}
+                value={this.state.email}
               ></TextInput>
             </View>
 
@@ -73,30 +130,40 @@ export default class RegisterScreen extends React.Component {
                 secureTextEntry
                 autoCapitalize="none"
                 style={styles.input}
-                onChangeText={(password) => this.setState({ password })}
-                value={this.state.password}
+                onChangeText={(repeatPassword) =>
+                  this.setState({ repeatPassword })
+                }
+                value={this.state.repeatPassword}
               ></TextInput>
             </View>
           </View>
 
+          <View style={styles.existentAccountError}>
+            <Text style={{ color: "#ff0000" }}>{this.state.registerError}</Text>
+          </View>
+
           <View>
             <TouchableOpacity
-              style={styles.loginButton}
-              onPress={() => this.props.navigation.navigate("Home")}
+              style={styles.registerButton}
+              onPress={() => this.register()}
             >
-              <Text style={{ color: "#FFF" }}>Login</Text>
+              {this.state.existAccountLoading ? (
+                <ActivityIndicator color="white"></ActivityIndicator>
+              ) : (
+                <Text style={{ color: "#FFF" }}>Create account</Text>
+              )}
             </TouchableOpacity>
           </View>
 
           <View>
             <TouchableOpacity
-              style={{ alignSelf: "center", marginTop: 32 }}
-              onPress={() => this.props.navigation.navigate("Register")}
+              style={{ alignSelf: "center", marginTop: 22, marginBottom: 20 }}
+              onPress={() => this.props.navigation.navigate("Login")}
             >
               <Text>
-                New to Buzz ?,{" "}
+                Already have an account ?,{" "}
                 <Text style={{ color: "#258e25", fontWeight: "700" }}>
-                  Sign Up here !
+                  Login here
                 </Text>
               </Text>
             </TouchableOpacity>
@@ -116,6 +183,7 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "400",
     textAlign: "center",
+    marginTop: 150,
   },
   form: {
     marginBottom: 48,
@@ -128,12 +196,40 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: "#258e25",
   },
-  loginButton: {
+  registerButton: {
     marginHorizontal: 30,
     backgroundColor: "#258e25",
     borderRadius: 4,
     height: 52,
     alignItems: "center",
     justifyContent: "center",
+  },
+  existentAccountError: {
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: -30,
+    marginBottom: 20,
+  },
+  greenCircle: {
+    width: 500,
+    height: 500,
+    position: "absolute",
+    zIndex: -3,
+    marginLeft: 100,
+    marginTop: -350,
+  },
+  yellowCircle: {
+    width: 400,
+    height: 400,
+    position: "absolute",
+    zIndex: -3,
+    marginLeft: -180,
+    marginTop: -330,
+  },
+  logo: {
+    position: "absolute",
+    alignSelf: "center",
+    marginTop: 100,
   },
 });
